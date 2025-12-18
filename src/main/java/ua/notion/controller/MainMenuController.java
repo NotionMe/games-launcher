579a356a81ea98b0833f90746e1dcb9287133f1c
NotionMe <c.ubohyi.stanislav@student.uzhnu.edu.ua>
2025-12-18 19:44:40 +0200

    realise drag and drop but not working

--- src/main/java/ua/notion/controller/MainMenuController.java
+++ src/main/java/ua/notion/controller/MainMenuController.java
@@ -1,6 +1,8 @@
 package ua.notion.controller;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,7 +10,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.DataFormat;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
@@ -23,6 +29,9 @@ import ua.notion.services.GameService;
 import ua.notion.services.IconService;
 
 public class MainMenuController {
+  @FXML
+  private StackPane centerLayer;
+
 
   private double xOffset = 0;
   private double yOffset = 0;
@@ -57,14 +66,6 @@ public class MainMenuController {
   private AnchorPane centerDropPane;
   @FXML
   private FlowPane cardContainer;
-  @FXML
-  private StackPane card;
-  @FXML
-  private ImageView cover;
-  @FXML
-  private ImageView icon;
-  @FXML
-  private Label title;
   @FXML
   private ScrollPane gamesScroll;
 
@@ -120,10 +121,59 @@ public class MainMenuController {
     Stage stage = (Stage) rootPane.getScene().getWindow();
 
     Optional<Game> game = gameService.addGameFromFile(user, stage);
+    game.ifPresent(g -> {
+      try {
+        gameService.createGameCard(g, cardContainer);
+      } catch (IOException e1) {
+      }
+    });
+  }
 
-    if (game.isPresent()) {
-      gameService.createGameCard(game.get(), cardContainer);
+  @FXML
+  private void fileViewDragDropped(DragEvent event) throws IOException {
+    
+    System.out.println(event.getDragboard().getFiles());
+
+
+
+    // if (db.hasFiles()) {
+    // File file = db.getFiles().get(0);
+
+    // if (file.isFile()) {
+    // Optional<Game> game = gameService.addGameFromArchive(user, file);
+    // game.ifPresent(g -> {
+    // try {
+    // gameService.createGameCard(g, cardContainer);
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // }
+    // });
+    // centerDropPane.setVisible(false);
+    // System.out.println("da");
+    // System.out.println("file: " + file);
+    // event.setDropCompleted(true);
+    // } else {
+    // centerDropPane.setVisible(true);
+    // System.out.println("file: " + file);
+    // System.out.println("nea - not a file");
+    // event.setDropCompleted(false);
+    // }
+    // } else {
+    // centerDropPane.setVisible(true);
+    // System.out.println("nea - no files");
+    // event.setDropCompleted(false);
+    // }
+
+    event.consume();
+  }
+
+  @FXML
+  private void fileViewDragOver(DragEvent event) {
+    if (event.getDragboard().hasFiles()) {
+      event.acceptTransferModes(TransferMode.COPY);
     }
+    System.out.println(event.getDragboard().getFiles());
+    event.consume();
   }
 
   @FXML
