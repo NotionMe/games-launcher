352ab7a365c0efb66d75fc0b7981ebb202149c9b
trrt54999 <moki22810sobakacom@gmail.com>
2025-12-28 01:34:53 +0200

    Updated window handler, implement full screen button in settings menu launcher

--- src/main/java/ua/notion/controllers/settings/LauncherPageController.java
+++ src/main/java/ua/notion/controllers/settings/LauncherPageController.java
@@ -1,13 +1,23 @@
 package ua.notion.controllers.settings;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import org.controlsfx.control.ToggleSwitch;
+import ua.notion.controllers.MainMenuController;
+import ua.notion.controllers.SettingsMenuController;
 import ua.notion.utils.Constants.UI;
+import ua.notion.utils.WindowHandler;
 
 public class LauncherPageController {
 
+  private WindowHandler windowHandler;
 
+  private MainMenuController mainMenuController;
+
+  private SettingsMenuController settingsMenuController;
 
   @FXML
   private StackPane rootPane;
@@ -26,5 +36,48 @@ public class LauncherPageController {
         getClass().getResource(UI.LAUNCHER_PAGE_CSS).toExternalForm(),
         getClass().getResource(UI.BASE_CSS).toExternalForm()
     );
+
+    setNoSteamSwitch();
+    setDisableAnimSwitch();
+  }
+
+  public void setParentController(SettingsMenuController controller) {
+    this.settingsMenuController = controller;
+
+    if (this.settingsMenuController != null && this.settingsMenuController.getMainWindowStage() != null) {
+      fullWindowSwitch.setSelected(settingsMenuController.getMainWindowStage().isFullScreen());
+
+      setFullWindowSwitchListener();
+    }
+  }
+
+  private void setFullWindowSwitchListener() {
+    fullWindowSwitch.selectedProperty().addListener((observable, oldValue, newValue) -> {
+      if (settingsMenuController != null && settingsMenuController.getMainWindowStage() != null) {
+        settingsMenuController.getMainWindowStage().setFullScreen(newValue);
+      }
+    });
+  }
+
+  private void setNoSteamSwitch() {
+    noSteamSwitch.selectedProperty().addListener((observable, oldValue,
+        newValue) -> {
+      if (newValue) {
+        System.out.println("Pressed");
+      } else {
+        System.out.println("FALSE");
+      }
+    });
+  }
+
+  private void setDisableAnimSwitch() {
+    disableAnimSwitch.selectedProperty().addListener((observable, oldValue,
+        newValue) -> {
+      if (newValue) {
+        System.out.println("Pressed");
+      } else {
+        System.out.println("FALSE");
+      }
+    });
   }
 }
