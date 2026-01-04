ce2f004f216f972e37dc155c280b149ccee567a4
trrt54999 <moki22810sobakacom@gmail.com>
2026-01-04 21:03:29 +0200

    Implement disable animation

--- src/main/java/ua/notion/ui/animation/AnimationHelper.java
+++ src/main/java/ua/notion/ui/animation/AnimationHelper.java
@@ -7,37 +7,50 @@ import javafx.util.Duration;
 import ua.notion.services.ImageService;
 
 public class AnimationHelper {
-    private final ImageService imageService = new ImageService();
 
-    public void transitionToBackground(String imagePath, ImageView imageView) {
-        try {
-            if (imagePath == null || imagePath.isBlank()) {
-                restoreDefaultBackground(imageView);
-                return;
-            }
+  private final ImageService imageService = new ImageService();
 
-            Image image = imageService.loadImage(imagePath, null, getClass());
+  public void transitionToBackground(String imagePath, ImageView imageView,
+      boolean animationsDisabled) {
+    try {
+      if (imagePath == null || imagePath.isBlank()) {
+        restoreDefaultBackground(imageView, animationsDisabled);
+        return;
+      }
 
-            if (image != null) {
-                imageView.setImage(image);
+      Image image = imageService.loadImage(imagePath, null, getClass());
 
-                FadeTransition ft = new FadeTransition(Duration.millis(200), imageView);
-                ft.setFromValue(0.0);
-                ft.setToValue(1.0);
-                ft.play();
-            } else {
-                restoreDefaultBackground(imageView);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+      if (image != null) {
+        imageView.setImage(image);
+
+        if (animationsDisabled) {
+          imageView.setOpacity(1.0);
+        } else {
+          FadeTransition ft = new FadeTransition(Duration.millis(200), imageView);
+
+          ft.setFromValue(0.0);
+          ft.setToValue(1.0);
+          ft.play();
         }
+      } else {
+        restoreDefaultBackground(imageView, animationsDisabled);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    public void restoreDefaultBackground(ImageView imageView) {
-        FadeTransition ft = new FadeTransition(Duration.millis(300), imageView);
-        ft.setFromValue(imageView.getOpacity());
-        ft.setToValue(0.0);
-        ft.play();
+  public void restoreDefaultBackground(ImageView imageView, boolean animationsDisabled) {
+
+    if (animationsDisabled) {
+      imageView.setOpacity(0.0);
+    } else {
+      FadeTransition ft = new FadeTransition(Duration.millis(300), imageView);
+
+      ft.setFromValue(imageView.getOpacity());
+      ft.setToValue(0.0);
+      ft.play();
     }
+  }
 
 }
